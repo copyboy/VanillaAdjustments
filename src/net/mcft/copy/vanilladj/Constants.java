83feb62ff54ac32d869fc0dc22b7d3aefaa7110b
copyboy <copyboy@hotmail.de>
2013-07-28 20:32:49 +0200

    Add constants and main mod class

---
+++ src/net/mcft/copy/vanilladj/Constants.java
@@ -0,0 +1,8 @@
+package net.mcft.copy.vanilladj;
+
+public class Constants {
+	
+	public static final String modId = "vanilladj";
+	public static final String modName = "VanillaAdjustments";
+	
+}
--- src/net/mcft/copy/vanilladj/VanillaAdjustments.java
+++ src/net/mcft/copy/vanilladj/VanillaAdjustments.java
@@ -1,7 +1,23 @@
 package net.mcft.copy.vanilladj;
 
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Constants.modId,
+     version = "@VERSION@",
+     useMetadata = true)
 public class VanillaAdjustments {
 	
+	@Instance(Constants.modId)
+	public static VanillaAdjustments instance;
 	
+	@EventHandler
+	public void load(FMLInitializationEvent event) {
+		
+		
+		
+	}
 	
 }
