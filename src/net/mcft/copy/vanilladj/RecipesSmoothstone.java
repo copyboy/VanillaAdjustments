01039f3687ae41a0e6c569506ee75231331c3822
copyboy <copyboy@hotmail.de>
2013-07-28 23:10:42 +0200

    Add RecipesSmoothstone

---
+++ src/net/mcft/copy/vanilladj/RecipesSmoothstone.java
@@ -0,0 +1,92 @@
+package net.mcft.copy.vanilladj;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.item.crafting.ShapedRecipes;
+import net.minecraft.item.crafting.ShapelessRecipes;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+import net.minecraftforge.oredict.ShapelessOreRecipe;
+
+public class RecipesSmoothstone {
+	
+	private static Object[] replaceIn = {
+		Item.swordStone,
+		Item.pickaxeStone,
+		Item.shovelStone,
+		Item.axeStone,
+		Item.hoeStone,
+		Block.dispenser,
+		Block.dropper,
+		Block.pistonBase,
+		Item.brewingStand
+	};
+	
+	private RecipesSmoothstone() {  }
+	
+	public static void initialize() {
+		
+		Item cobblestone = Item.itemsList[Block.cobblestone.blockID];
+		Item smoothstone = Item.itemsList[Block.stone.blockID];
+		
+		Set<Item> replaceItems = new HashSet<Item>(replaceIn.length);
+		for (Object obj : replaceIn) {
+			if (obj instanceof Item)
+				replaceItems.add((Item)obj);
+			else if (obj instanceof Block)
+				replaceItems.add(Item.itemsList[((Block)obj).blockID]);
+		}
+		
+		VanillaAdjustments.log.info("Replacing cobblestone with smoothstone in recipes of " + replaceItems.size() + " items.");
+		
+		List<IRecipe> recipes = CraftingManager.getInstance().getRecipeList();
+		for (IRecipe recipe : recipes) {
+			ItemStack output = recipe.getRecipeOutput();
+			if ((output == null) || !replaceItems.contains(output.getItem())) continue;
+			
+			if (recipe instanceof ShapedRecipes) {
+				
+				ShapedRecipes shapedRecipe = (ShapedRecipes)recipe;
+				ItemStack[] items = shapedRecipe.recipeItems;
+				for (int i = 0; i < items.length; i++)
+					if ((items[i] != null) && (items[i].getItem()) == cobblestone)
+						items[i] = new ItemStack(smoothstone);
+				
+			} else if (recipe instanceof ShapelessRecipes) {
+				
+				ShapelessRecipes shapelessRecipe = (ShapelessRecipes)recipe;
+				List<ItemStack> items = shapelessRecipe.recipeItems;
+				for (int i = 0; i < items.size(); i++)
+					if (items.get(i).getItem() == cobblestone)
+						items.set(i, new ItemStack(smoothstone));
+				
+			} else if (recipe instanceof ShapedOreRecipe) {
+				
+				ShapedOreRecipe shapedOreRecipe = (ShapedOreRecipe)recipe;
+				Object[] items = shapedOreRecipe.getInput();
+				for (int i = 0; i < items.length; i++)
+					if ((items[i] instanceof ItemStack) &&
+					    (((ItemStack)items[i]).getItem() == cobblestone))
+						items[i] = new ItemStack(smoothstone);
+				
+			} else if (recipe instanceof ShapelessOreRecipe) {
+				
+				ShapelessOreRecipe shapelessRecipe = (ShapelessOreRecipe)recipe;
+				List items = shapelessRecipe.getInput();
+				for (int i = 0; i < items.size(); i++)
+					if ((items.get(i) instanceof ItemStack) &&
+					    (((ItemStack)items.get(i)).getItem() == cobblestone))
+						items.set(i, new ItemStack(smoothstone));
+				
+			}
+		}
+		
+	}
+	
+}
--- src/net/mcft/copy/vanilladj/VanillaAdjustments.java
+++ src/net/mcft/copy/vanilladj/VanillaAdjustments.java
@@ -1,9 +1,12 @@
 package net.mcft.copy.vanilladj;
 
+import java.util.logging.Logger;
+
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Constants.modId,
      version = "@VERSION@",
@@ -13,10 +16,19 @@ public class VanillaAdjustments {
 	@Instance(Constants.modId)
 	public static VanillaAdjustments instance;
 	
+	public static Logger log;
+	
 	@EventHandler
-	public void load(FMLInitializationEvent event) {
+	public void preInit(FMLPreInitializationEvent event) {
 		
+		log = event.getModLog();
 		
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		
+		RecipesSmoothstone.initialize();
 		
 	}
 	
